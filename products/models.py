44dc0b12cb39215f231ab5a67a840eb0f96c5366
amiradp <amiradp3@gmail.com>
2024-06-04 19:59:12 +0330

    new apps and add models for product

--- products/models.py
+++ products/models.py
@@ -1,42 +1,28 @@
-# from django.db import models
+from django.db import models
 
-# from django.urls import reverse
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
 
-
-# class Category(models.Model):
-#     sub_category = models.ForeignKey('self', on_delete=models.CASCADE, related_name='sub_categories', null=True, blank=True)
-#     is_sub = models.BooleanField(default=False)
-#     name = models.CharField(max_length=200)
-#     slug = models.SlugField(max_length=200, unique=True)
-
-#     class Meta:
-#         ordering = ('name',)
-#         verbose_name = 'category'
-#         verbose_name_plural = 'categories'
-
-#     def __str__(self):
-#         return self.name
-
-#     def get_absolut_url(self):
-#         return reverse('shop:category_filter', args={self.slug})
+    def __str__(self):
+        return self.name
 
 
-# class Product(models.Model):
-#     category = models.ManyToManyField(Category, related_name='products')
-#     name = models.CharField(max_length=200)
-#     slug = models.SlugField(max_length=200)
-#     image = models.ImageField(upload_to='products/%Y/%b/%d/')
-#     description = models.TextField()
-#     price = models.IntegerField()
-#     status = models.BooleanField(default=True)
-#     created = models.DateTimeField(auto_now_add=True)
-#     updated = models.DateTimeField(auto_now=True)
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
 
-#     class Meta:
-#         ordering = ('name',)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
 
-#     def __str__(self):
-#         return self.name
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
 
-#     def get_absolut_url(self):
-#         return reverse('shop:product_detail', args={self.slug, })
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+        
--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
-urlpatterns = [
+from django.conf import settings
+from . import views
 
-]
+urlpatterns = [
+    path('', views.product_list, name='product_list'),
+    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+    # path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
